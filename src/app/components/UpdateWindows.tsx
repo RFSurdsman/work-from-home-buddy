740646a1dc1992fb96dd60caca2b7d1b9c532445
George <georgefidler1709@gmail.com>
2020-08-29 16:57:58 +1000

    quick fix for windows so if you don't have any previous tabs open you'll still see the dashboard

--- src/app/components/UpdateWindows.tsx
+++ src/app/components/UpdateWindows.tsx
@@ -2,7 +2,7 @@ import { WindowInfo } from "../types";
 import _ from "lodash";
 
 export const updateWindows = (savedWindows: WindowInfo[], setSavedWindows: (windows: WindowInfo[]) => void) => {
-    
+
     chrome.windows.getAll({populate: true}, (windows) => {
       const saved_windows: WindowInfo[] = windows.map(window => {
         const window_state = _.pick(window, ['height', 'width', 'top', 'left', 'type', 'state']);
@@ -10,16 +10,18 @@ export const updateWindows = (savedWindows: WindowInfo[], setSavedWindows: (wind
                       .map(tab => tab.url!)
                       .filter(url => !url.startsWith("chrome://newtab"));
         chrome.windows.remove(window.id);
-        console.log(urls)
         return {...window_state, url: urls};
       });
-  
-      savedWindows.forEach((window: WindowInfo, i: Number) => {
+      
+      (savedWindows) ?
+        savedWindows.forEach((window: WindowInfo, i: Number) => {
           (i === savedWindows.length - 1) ?
             chrome.windows.create({...window, url: ["chrome://newtab", ...window.url]})
           :
             chrome.windows.create({...window})
-      })
+        })
+      :
+        chrome.windows.create({});
   
       setSavedWindows(saved_windows);
     });

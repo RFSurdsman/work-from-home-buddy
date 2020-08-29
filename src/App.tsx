5adcdbe0d3e3c5aed363ae5012d7f699bfb78984
Tony Tang <tonytang.dev@gmail.com>
2020-08-29 11:37:42 +1000

    Add current time to dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import createPersistedState from "use-persisted-state";
 const commonTheme = {
   font: {
     family: "Roboto",
-    size: "18px",
+    size: "36px",
     height: "20px",
   },
 };
@@ -15,7 +15,7 @@ const HomeModeTheme = {
   global: {
     ...commonTheme,
     colors: {
-      brand: "rgb(50, 250, 200)",
+      brand: "rgb(40, 230, 150)",
       secondary: "rgb(50, 150, 250)",
       text: "#ebebeb",
     },
@@ -65,10 +65,20 @@ export const NewTabApp = () => {
   const useWorkModeState = createPersistedState("workMode");
   const [isWorkMode, setIsWorkMode] = useWorkModeState(false);
 
+  const [time, setTime] = useState(Date.now());
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(Date.now());
+    }, 1000);
+  });
+
   return (
     <Grommet theme={isWorkMode ? WorkModeTheme : HomeModeTheme} full>
       <Box fill align="center" justify="center" background="brand">
-        <h1>WFH Buddy</h1>
+        <h6>WFH Buddy</h6>
+        <h3>Good morning, Tony</h3>
+        <h1>{new Date(time).toLocaleTimeString()}</h1>
         <Button
           primary
           label={isWorkMode ? " End work mode" : "Start work mode"}

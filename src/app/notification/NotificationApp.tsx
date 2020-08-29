29149f581cc97b6826415e303b7daaae1ad01139
Ryan Fallah <ryan_fallah@hotmail.com>
2020-08-29 18:08:37 +1000

    Pomdoro

--- src/app/notification/NotificationApp.tsx
+++ src/app/notification/NotificationApp.tsx
@@ -1,15 +1,32 @@
 import React from "react";
-import { HomeModeTheme, WorkModeTheme } from "../styles/themes";
-import createPersistedState from "use-persisted-state";
-import { Box, Grommet } from "grommet";
+import {
+  Box,
+  Grommet,
+  Card,
+  CardHeader,
+  CardBody,
+  Meter,
+  Button,
+} from "grommet";
+
+const theme = {
+  global: {
+    font: {
+      family: "Roboto",
+    },
+    colors: {
+      brand: "rgb(40, 230, 150)",
+      secondary: "rgb(50, 150, 250)",
+      text: "#ebebeb",
+    },
+  },
+};
 
 const NotificationApp = () => {
-  const useWorkModeState = createPersistedState("workMode");
-  const [isWorkMode, setIsWorkMode] = useWorkModeState(false);
-
   return (
-    <Grommet theme={isWorkMode ? WorkModeTheme : HomeModeTheme}>
-      <Box
+    <Grommet theme={theme}>
+      <Card
+        pad="10px"
         height="200px"
         width="400px"
         align="center"
@@ -17,8 +34,32 @@ const NotificationApp = () => {
         background="brand"
         round="large"
       >
-        <h4>NOTIFICATION</h4>
-      </Box>
+        <CardHeader pad="small">
+          <b>Pomdoro Notification</b>
+        </CardHeader>
+        <CardBody pad="medium" direction="row">
+          <Box align="center" justify="center" height="100%" width="50%">
+            <h2 style={{ textAlign: "center" }}>Time for a break soon!</h2>
+            <Button primary label="Start" color="secondary" />
+          </Box>
+          <Box align="center" justify="center" height="100%" width="50%">
+            <Meter
+              values={[
+                {
+                  value: 60,
+                  label: "sixty",
+                  color: "rgb(50, 200, 250)",
+                  onClick: () => {},
+                },
+              ]}
+              round
+              type="circle"
+              aria-label="meter"
+              thickness="large"
+            />
+          </Box>
+        </CardBody>
+      </Card>
     </Grommet>
   );
 };
